f62faef29ecbe456d067d3a5900f3cb074536922
manouman <manurbano.dev@gmail.com>
2016-01-21 15:09:03 -0300

    Esquemas de datos para Camas y Mapa de Camas definidos
    Definición de esquemas de datos en mongoose para los modelos de mapa de
    camas y camas.
    Tarea en Taiga:
    https://tree.taiga.io/project/jgabriel-internacion/task/3

---
+++ models/Camas.js
@@ -0,0 +1,54 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+    habitacion: Number,
+    numero: Number,
+    tipoCama: {
+        type: String,
+        enum: ['cama', 'sillon', 'cuna'],
+        required: true,
+        default: 'cama'
+    },
+    oxigeno: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    desinfectada: {
+        type: Boolean,
+        required: false,
+        default: true
+    },
+    estado: {
+        type: String,
+        enum: ['ocupada', 'desocupada', 'reparacion'],
+        required: true,
+        default: 'desocupada'
+    },
+    idInternacion:  {
+        type: Schema.Types.ObjectId,
+        ref: 'Internacion'
+    },
+    paciente: {
+        id: {
+            type: Schema.Types.ObjectId,
+            ref: 'Persona'
+        },
+        apellido: String,
+        nombre: String,
+        documento: Number,
+        fechaNacimiento: Date,
+        sexo: {
+            type: String,
+            enum: ['Masculino', 'Femenino', 'Indeterminado']
+        }
+    }
+});
+
+schema.set('toJSON', {
+    virtuals: true,
+    versionKey: false,
+});
+
+module.exports = mongoose.model('Cama', schema);
