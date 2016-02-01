affe9858c28561e8cf604a2830a6e8bc7e24e57f
jfgabriel <jgabriel@hospitalneuquen.org.ar>
2016-02-01 09:36:28 -0300

    Actualizar app.js

--- app.js
+++ app.js
@@ -2,40 +2,39 @@ var express = require('express'),
     logger = require('morgan'),
     bodyParser = require('body-parser'),
     path = require('path'),
+    app = express(),
     mongoose = require("mongoose"),
-    requireDir = require('require-dir'),
-    cors = require('cors');
+    requireDir = require('require-dir');
 
-// Express config
-var app = express();
-app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-// View engine setup
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// Routes
+// routes
 var routes = requireDir('./routes/');
 for (var route in routes)
     app.use('/', routes[route]);
 
-// Connect to MongoDB
+// connect
 mongoose.connect("mongodb://desarrollo:27017/hospital");
 
-// Error handlers
-// ... 404
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-// ... development
+// error handlers
+
+// development error handler
+// will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
@@ -46,7 +45,8 @@ if (app.get('env') === 'development') {
     });
 }
 
-// ... production
+// production error handler
+// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
